40917f61865565e54330e3182c3d102ad3f6ac93
Thiago-CSantos <thiagocarretero433@gmail.com>
2023-12-15 14:35:40 -0300

    ex002

---
+++ ex002/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>PHP</title>
+</head>
+
+<body>
+      <h1>Exemplo PHP</h1>
+      <?php
+            echo "Hoje é dia " . date("d/M/Y");
+            echo "hora" . date("G:i:s T");
+      ?>
+
+      <p>tentando me livrar da maldição</p>
+</body>
+
+</html>
