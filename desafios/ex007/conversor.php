1d0723be231820f5bf627e312e14ce46629e2a09
Thiago-CSantos <thiagocarretero433@gmail.com>
2023-12-17 13:10:34 -0300

    feat: formatação de moedas com internacionalização

--- desafios/ex007/conversor.php
+++ desafios/ex007/conversor.php
@@ -10,24 +10,32 @@
 
 <body>
       <header>
-            <h1>Resultado do processamento</h1>
+            <h1>Resultado da conversão</h1>
       </header>
 
       <main>
             <?php
             $valorReal = $_GET["valorReal"];
-            var_dump($valorReal);
-            $valorCambio = "0.20";
+            // var_dump($valorReal);
+            $cotacao = 4.94;
 
-            $valorDolar = $valorReal * $valorCambio;
+            $valorDolar = $valorReal / $cotacao;
 
-            echo "<p>Seus R$ $valorReal equivalente a $valorDolar</p>"
+            echo "<p>Seus R\$" . number_format($valorReal, 2, ",", ".") . " equivalente a US\$ " . number_format($valorDolar, 2, ",", ".") . "</p>"
             ?>
             <p>*<strong>Cotação fixa de R$ 4,94</strong> informarda diretamente no codigo*</p>
+
+            <?php 
+                  //formatação de moedas com internacionalização!
+                  $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+
+                  echo "Seus ". numfmt_format_currency($padrao, $valorReal, "BRL"). "equivalente a ". numfmt_parse_currency($padrao, $valorDolar, "USD");
+            ?>
+
             <input type="button" value="Voltar" onclick="voltar()">
 
             <script>
-                  function voltar(){
+                  function voltar() {
                         return history.go(-1);
                   }
             </script>
